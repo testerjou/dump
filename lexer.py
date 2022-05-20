741196f4f471a64363673a2eab376a086b9e5598
testerjou <105911585+testerjou@users.noreply.github.com>
2022-05-20 17:46:30 +0300

    11

--- lexer.py
+++ lexer.py
@@ -1,11 +1,10 @@
 class Lexer: 
-    def __init__(self, input) -> None:
+    def __init__(self, input):
         self.input = input.lower()
         pass
     
     KEYWORDS = ['eteen', 'taakse', 'oikealle', 'vasemmalle']
     DIGITS = '0123456789'
-    LETTERS = 'abcdefghjiklmnopqrstuvxyzåöä'
 
     def get_next(self):
         pass
@@ -15,7 +14,7 @@ class Lexer:
 
         print("Input",self.input)
         split_list = [element.lower() for element in input.split()]
-
+        print("Split:",split_list)
         for element in split_list:
             if element in self.KEYWORDS:
                 token_list.append(("KEYWORD",element))
@@ -25,53 +24,15 @@ class Lexer:
                 print("NOT FOUND ", element)
         print("TOKENS:",token_list)
 
-
-    def create_tokens1(self):
-
-        token_list = []
-        current_word = ""
-        current_char = ""
-
-        #i = index, v = value
-
-        print("Input",self.input)
-
-        split_list = input.split()
-
-        print("split_list", split_list)
-
-        for i, v in enumerate(self.input):
-            #if i + 1 == len(input):
-            #    break
-            
-            if v in self.LETTERS:
-                current_word += v
-
-            if v in self.DIGITS:
-                current_char = v 
-
-            if v.isspace():
-                #checkword
-                print(current_word)
-                
-
-                if current_word in self.KEYWORDS:
-                    token_list.append(("KEYWORD",current_word))
-                if current_char in self.DIGITS:
-                    token_list.append(("INT", current_char))
-                current_word = ""
-                current_char = ""
-            pass
-
-        print(token_list)
-
-        for i in range(len(input)):
-            pass
-        
-
-        pass
+    def set_input(self, input):
+        self.input = input
 
 input = "Eteen 5 Taakse 5"
 lex = Lexer(input)
+lex.create_tokens()
 
+lex.set_input("Eteen5 Taakse5")
+lex.create_tokens()
+
+lex.set_input("Eteen5Taakse5")
 lex.create_tokens()
