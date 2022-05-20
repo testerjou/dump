ad2c46c9110f4465137357d2d9f24e5b3fa8db26
testerjou <105911585+testerjou@users.noreply.github.com>
2022-05-20 15:01:56 +0300

    3

--- lexer.py
+++ lexer.py
@@ -7,11 +7,39 @@ class Lexer:
     DIGITS = '0123456789'
     LETTERS = 'abcdefghjiklmnopqrstuvxyzåöä'
 
-    def advance():
+    def get_next(self):
+        pass
+    def create_tokens(self):
+
+        token_list = []
         current_word = ""
         current_char = ""
+
+        #i = index, v = value
+        for i, v in enumerate(input):
+            #if i + 1 == len(input):
+            #    break
+            
+            if v in self.LETTERS:
+                current_word += v
+
+            if v.isspace():
+                print("Works", " value ", v, "123")
+                #checkword
+                print(current_word)
+                current_word = ""
+            
+            pass
+
+        for i in range(len(input)):
+            pass
+        
+
+        pass
 
 input = "Eteen 5 Taakse 5"
 lex = Lexer(input)
 print(lex.input)
 print("Hello")
+
+lex.create_tokens()
