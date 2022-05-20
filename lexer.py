80a974dac50837e241b494b3fc2febcc0520160c
testerjou <105911585+testerjou@users.noreply.github.com>
2022-05-20 12:52:17 +0300

    2

--- lexer.py
+++ lexer.py
@@ -1,13 +1,17 @@
 class Lexer: 
     def __init__(self, input) -> None:
-
         self.input = input.lower()
-
         pass
     
     KEYWORDS = ['eteen', 'taakse', 'oikealle', 'vasemmalle']
     DIGITS = '0123456789'
     LETTERS = 'abcdefghjiklmnopqrstuvxyzåöä'
 
+    def advance():
+        current_word = ""
+        current_char = ""
 
+input = "Eteen 5 Taakse 5"
+lex = Lexer(input)
+print(lex.input)
 print("Hello")
