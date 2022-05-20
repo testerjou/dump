a78e1986fa89470ab0d5db901f75ad0e5ee549a5
testerjou <105911585+testerjou@users.noreply.github.com>
2022-05-20 12:22:08 +0300

    Create lexer.py

---
+++ lexer.py
@@ -0,0 +1,10 @@
+class Lexer: 
+    def __init__(self, input) -> None:
+
+        self.input = input.lower()
+
+        pass
+    
+    KEYWORDS = ['eteen', 'taakse', 'oikealle', 'vasemmalle']
+    DIGITS = '0123456789'
+    LETTERS = 'abcdefghjiklmnopqrstuvxyzåöä'
